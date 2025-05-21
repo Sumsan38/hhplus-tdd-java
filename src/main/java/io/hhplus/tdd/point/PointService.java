524c942fda9b741eef8a294ee039de533529c96a
Sumsan38 <ndanl4647@gmail.com>
2025-05-21 18:18:30 +0900

    style: add a comment to the chargeUserPoint method.

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -24,6 +24,16 @@ public class PointService {
     }
 
 
+    /**
+     * 지정된 유저 ID와 해당 충전 금액을 사용하여 사용자의 포인트를 충전합니다.
+     * 만약 금액이 허용된 최대 포인트를 초과하면 예외를 발생시킵니다.
+     * 충전 후 업데이트된 사용자 포인트 데이터를 반환하며, 충전 내역은 기록됩니다.
+     *
+     * @param id 포인트를 충전할 사용자의 고유 식별자
+     * @param amount 충전할 포인트 금액, 최대 충전 금액은 제한됩니다
+     * @return 업데이트된 사용자 포인트의 데이터를 포함한 UserPoint 객체
+     * @throws IllegalArgumentException 입력된 충전 금액이 최대 포인트 한도를 초과하는 경우
+     */
     public UserPoint chargeUserPoint(long id, long amount) {
         if(amount >= MAX_POINT) {
             throw new IllegalArgumentException("유저의 포인트 초과입니다.");
