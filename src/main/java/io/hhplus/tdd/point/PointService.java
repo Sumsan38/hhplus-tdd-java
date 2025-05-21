9e659529d847a8c5d5e9008e450c3ae077c8b963
Sumsan38 <ndanl4647@gmail.com>
2025-05-21 17:10:43 +0900

    test: Add point charging logic and its unit test(red)
    - Introduced the `chargeUserPoint` method in `PointService` to handle user point charging, with validation and updates.

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -1,5 +1,6 @@
 package io.hhplus.tdd.point;
 
+import io.hhplus.tdd.database.PointHistoryTable;
 import io.hhplus.tdd.database.UserPointTable;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,10 +10,24 @@ import org.springframework.stereotype.Service;
 public class PointService {
 
     private final UserPointTable userPointTable;
+    private final PointHistoryTable pointHistoryTable;
 
+    private static final Long MAX_POINT = 100_000_000L;
+
+    /**
+     * ID로 특정 사용자의 포인트 정보를 검색합니다.
+     * @param id 포인트 정보를 검색 할 사용자의 고유 식별자
+     * @return 사용자의 포인트 데이터가 포함 된 사용자 포인트 인스턴스
+     */
     public UserPoint getUserPoint(long id) {
         return userPointTable.selectById(id);
     }
 
 
+    public UserPoint chargeUserPoint(long id, long amount) {
+        UserPoint userPoint = userPointTable.selectById(id);
+
+
+        return null;
+    }
 }
--- src/test/java/io/hhplus/tdd/point/PointServiceTest.java
+++ src/test/java/io/hhplus/tdd/point/PointServiceTest.java
@@ -1,5 +1,6 @@
 package io.hhplus.tdd.point;
 
+import io.hhplus.tdd.database.PointHistoryTable;
 import io.hhplus.tdd.database.UserPointTable;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,17 +15,41 @@ class PointServiceTest {
     void getUserPoint_ReturnValidId() {
         // given
         UserPointTable userPointTable = mock(UserPointTable.class);
-        PointService pointService = new PointService(userPointTable);
+        PointHistoryTable historyTable = mock(PointHistoryTable.class);
+        PointService pointService = new PointService(userPointTable, historyTable);
         long id = 1L;
 
-        when(userPointTable.selectById(id)).thenReturn(new UserPoint(id, 100, 1000L));
+        when(userPointTable.selectById(id)).thenReturn(new UserPoint(id, 100L, System.currentTimeMillis()));
         
         // when
         UserPoint userPoint = pointService.getUserPoint(id);
 
         // then
         assertThat(userPoint.id()).isEqualTo(id);
+        assertThat(userPoint.point()).isEqualTo(100L);
         verify(userPointTable, times(1)).selectById(id);
+    }
+
+    @Test
+    @DisplayName("특정 유저의 포인트 충전을 성공한다")
+    void chargeUserPoint_withValidUserIdAndPoint() {
+        // given
+        UserPointTable userPointTable = mock(UserPointTable.class);
+        PointHistoryTable historyTable = mock(PointHistoryTable.class);
+        PointService pointService = new PointService(userPointTable, historyTable);
+        long id = 1L;
+        long amount = 100L;
+
+        when(userPointTable.selectById(id)).thenReturn(new UserPoint(id, 100L, System.currentTimeMillis()));
+
+        // when
+        UserPoint userPoint = pointService.chargeUserPoint(id, amount);
+
+        // then
+        assertThat(userPoint.id()).isEqualTo(id);
+        assertThat(userPoint.point()).isEqualTo(200);
+        verify(userPointTable, times(1)).selectById(id);
+        verify(userPointTable, times(1)).insertOrUpdate(id, anyLong());
     }
 
 }
