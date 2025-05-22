dc507398b8df34f382d2371fec48b9202ab3af4b
Sumsan38 <ndanl4647@gmail.com>
2025-05-22 18:05:16 +0900

    refactor: Refactor PointServiceTest to use @BeforeEach for setup.

--- src/test/java/io/hhplus/tdd/point/PointServiceTest.java
+++ src/test/java/io/hhplus/tdd/point/PointServiceTest.java
@@ -2,6 +2,7 @@ package io.hhplus.tdd.point;
 
 import io.hhplus.tdd.database.PointHistoryTable;
 import io.hhplus.tdd.database.UserPointTable;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,13 +14,21 @@ import static org.mockito.Mockito.*;
 
 class PointServiceTest {
 
+    private UserPointTable userPointTable;
+    private PointHistoryTable historyTable;
+    private PointService pointService;
+
+    @BeforeEach
+    void setUp() {
+        userPointTable = mock(UserPointTable.class);
+        historyTable = mock(PointHistoryTable.class);
+        pointService = new PointService(userPointTable, historyTable);
+    }
+
     @Test
     @DisplayName("특정 유저의 포인트를 조회합니다.")
     void getUserPoint_ReturnValidId() {
         // given
-        UserPointTable userPointTable = mock(UserPointTable.class);
-        PointHistoryTable historyTable = mock(PointHistoryTable.class);
-        PointService pointService = new PointService(userPointTable, historyTable);
         long id = 1L;
 
         when(userPointTable.selectById(id)).thenReturn(new UserPoint(id, 100L, System.currentTimeMillis()));
@@ -37,9 +46,6 @@ class PointServiceTest {
     @DisplayName("특정 유저의 포인트 충전을 성공하고 히스토리를 저장한다.")
     void chargeUserPoint_withValidUserIdAndPoint_AndSaveHistory() {
         // given
-        UserPointTable userPointTable = mock(UserPointTable.class);
-        PointHistoryTable historyTable = mock(PointHistoryTable.class);
-        PointService pointService = new PointService(userPointTable, historyTable);
         long id = 1L;
         long beforePoint = 100L;    // 업데이트 전 포인트
         long amount = 100L;         // 충전 포인트
@@ -66,9 +72,6 @@ class PointServiceTest {
     @DisplayName("특정 유저의 포인트 충전이 실패합니다(최대 잔고 초과).")
     void chargeUserPoint_withOverPoint() {
         // given
-        UserPointTable userPointTable = mock(UserPointTable.class);
-        PointHistoryTable historyTable = mock(PointHistoryTable.class);
-        PointService pointService = new PointService(userPointTable, historyTable);
         long id = 1L;
         long beforePoint = 1L;
         long amount = Long.MAX_VALUE;
@@ -87,9 +90,6 @@ class PointServiceTest {
     @DisplayName("특정 유저의 포인트를 사용을 성공합니다.")
     void useUserPoint_withValidUserIdAndPoint() {
         // given
-        UserPointTable userPointTable = mock(UserPointTable.class);
-        PointHistoryTable historyTable = mock(PointHistoryTable.class);
-        PointService pointService = new PointService(userPointTable, historyTable);
         long id = 1L;
         long amount = 100L;
         long useAmount = 100L;
@@ -114,9 +114,6 @@ class PointServiceTest {
     @DisplayName("특정 유저의 포인트 사용이 실패합니다(사용 포인트 초과).")
     void useUserPoint_withOverPoint() {
         // given
-        UserPointTable userPointTable = mock(UserPointTable.class);
-        PointHistoryTable historyTable = mock(PointHistoryTable.class);
-        PointService pointService = new PointService(userPointTable, historyTable);
         long id = 1L;
         long amount = 100L;
 
@@ -131,9 +128,6 @@ class PointServiceTest {
     @DisplayName("특정 유저의 포인트 히스토리를 조회합니다.")
     void getUserPointHistory() {
         // given
-        UserPointTable userPointTable = mock(UserPointTable.class);
-        PointHistoryTable historyTable = mock(PointHistoryTable.class);
-        PointService pointService = new PointService(userPointTable, historyTable);
         long id = 1L;
 
         List<PointHistory> historyList = List.of(new PointHistory[]{
@@ -157,7 +151,8 @@ class PointServiceTest {
         verify(historyTable, times(1)).selectAllByUserId(id);
     }
 
-    private void assertPointHistory(PointHistory history, long expectedUserId, long expectedAmount, TransactionType expectedType) {
+    private void assertPointHistory(PointHistory history,
+                                    long expectedUserId, long expectedAmount, TransactionType expectedType) {
         assertThat(history.userId()).isEqualTo(expectedUserId);
         assertThat(history.amount()).isEqualTo(expectedAmount);
         assertThat(history.type()).isEqualTo(expectedType);
